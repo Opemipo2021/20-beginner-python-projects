bcfd71cd66a80e7125a4ae0445fe500e6bbb3042
Opemipo20201 <omoimole2020@gmail.com>
2022-12-15 13:37:23 +0100

    Madlib adventure project

---
+++ madlib.py
@@ -0,0 +1,6 @@
+noun1 = input("Enter the noun: ")
+adjective = input("Enter adjective: ")
+noun2 = input("Enter noun: ")
+
+print(f"After hiding the painting in his {noun1} for two years, he grew {adjective} and tried to sell it "
+      f"to a/an {noun2} in Florence, but was caught.")
